f457e0eda8be7ee8e02dfe9dc084dda5b1a5bd0b
Gwen <gwen@Gwens-MacBook-Air.local>
2026-01-07 17:17:58 +0800

    Changed api url

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  API_URL: 'https://streakchain-api-production.up.railway.app',
+  API_URL: 'https://streakchain-api.vercel.app',
   GOOGLE_CLIENT_ID: '787127394648-s4ogouu423m5hf7lp4cl33coomugp0aj.apps.googleusercontent.com',
 };
